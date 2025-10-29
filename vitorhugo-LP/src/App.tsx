0dce58b214cc9920e292bf4fc3ac6ec981f8b147
Thr0ughTheFloor <contatofabio2005@gmail.com>
2025-10-29 18:29:50 -0300

    Att Nav + Banner

--- vitorhugo-LP/src/App.tsx
+++ vitorhugo-LP/src/App.tsx
@@ -1,33 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import testeBanner from './assets/images/testeBanner.jpg'
+import menuBtn from "./assets/images/menu.png"
+import closeBtn from './assets/images/close.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [btn,setBtn] = useState(false);
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className='w-full'>
+        <header className='flex w-full h-30 bg-(--amarelo-queimado)'>
+          <nav className='flex w-full flex-col md:flex-row justify-center items-center md:gap-50'>           
+            <div className='hidden md:flex h-full w-[60%] md:w-[55rem] ml-10 justify-center items-center md:ml-0'>
+              <h1>Vitor Hugo</h1>
+            </div>
+            {/* Nav Desktop */}
+            <div className='hidden md:flex w-[50%]'>
+              <ul className='flex gap-15'>
+                <li><a>opcao 1</a></li>
+                <li><a>Opcao 2</a></li>
+                <li><a>Opcao 3</a></li>
+                <li><a>Opcao 4</a></li>
+              </ul>
+            </div>
+
+            <div className='md:hidden flex h-full w-full justify-center items-center'>
+              <button onClick={() => setBtn(!btn)}><img src={btn ? closeBtn : menuBtn} width={btn ? 20 : 30}/></button>
+            </div>
+
+            {/* Nav Mobile */}
+          { btn && (
+            <div className='absolute z-999 md:hidden w-full h-50 mt-70'>
+              <div className=' bg-(--amarelo-queimado-escuro) p-10 inset-shadow-sm inset-shadow-900'>
+                <ul className='flex gap-15 flex-col items-center'>
+                <li><a>opcao 1</a></li>
+                <li><a>Opcao 2</a></li>
+                <li><a>Opcao 3</a></li>
+                <li><a>Opcao 4</a></li>
+              </ul>
+              </div>
+            </div>
+          )}
+          </nav>
+        </header>
+        {/* Banner Abaixo*/}
+        <div className='flex w-full max-h-180 drop-shadow-xl/10'>
+          <img src={testeBanner} alt="imgTeste" className='relative w-full object-cover object-top-right contrast-50' />
+        </div>
+      </section>
     </>
   )
 }
